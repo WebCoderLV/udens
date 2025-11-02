3df19366bef95f9f9c6a039616593797c9834819
Rutaer <arturser@yahoo.com>
2025-11-02 20:51:56 +0200

    Signāla forma strādā.

--- src/app/pudeles/pudeles.ts
+++ src/app/pudeles/pudeles.ts
@@ -1,28 +1,51 @@
 import { Component, inject, signal } from '@angular/core';
+import { applyEach, Field, form, required } from '@angular/forms/signals';
 import { UdensServiss } from '../servisi/udens-serviss';
 import { Pudele } from '../maodeli/pudele';
-import { form } from '@angular/forms/signals';
+
+type Daudzpudeles = {
+  pudeluArrajs: Pudele[];
+}
 
 @Component({
   selector: 'app-pudeles',
   standalone: true,
+  imports: [Field],
   templateUrl: './pudeles.html',
   styleUrl: './pudeles.css',
 })
 export class Pudeles {
 
-  udensServiss = inject(UdensServiss);
+  private udensServiss = inject(UdensServiss);
 
-  visasPudeles() {
-    this.udensServiss.iegutPudeles();
+  private createTuksaPudele(): Pudele {
+    return {
+      materials: '',
+      volume: 0,
+      garsa: '',
+      kategorija: false
+    };
   }
 
-  pudelesForma = form(signal<Pudele>({
-    materials: '',
-    volume: 0,
-    garsa: '',
-    kategorija: false
-  }))
+  protected pudelesSignals = signal<Daudzpudeles>({
+    pudeluArrajs: [this.createTuksaPudele()]
+  });
 
+  pudelesForma = form(this.pudelesSignals, (formasPath) => {
+    applyEach(formasPath.pudeluArrajs, (arajaElementaPath) => {
+      required(arajaElementaPath.materials, { message: 'Materiāls ir obligāts lauks' });
+    });
+  });
+
+  pievienotPudeli(): void {
+    this.pudelesSignals.update((data) => ({
+      pudeluArrajs: [...data.pudeluArrajs, this.createTuksaPudele()]
+    }));
+  }
+  nonemtPudeli(index: number): void {
+    this.pudelesSignals.update((data) => ({
+      pudeluArrajs: data.pudeluArrajs.filter((_, i) => i !== index)
+    }));
+  }
 
 }
--- src/app/servisi/udens-serviss.ts
+++ src/app/servisi/udens-serviss.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Pudele } from '../maodeli/pudele';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,15 +14,22 @@ export class UdensServiss {
 
 
   iegutPudeles() {
-    this.http.get<Pudele[]>(`${this.URL}api/pudeles`).subscribe({
+    return this.http.get<Pudele[]>(`${this.URL}api/pudeles`).subscribe({
       next: (pudeles: Pudele[]) => {
         console.log('Pudeles iegutas sekmigi:', pudeles);
+        return pudeles;
       },
       error: (err: any) => {
         console.error('Kļūda iegūstot pudeles:', err);
       }
-    })
+    });
+  }
 
+  pievienotPudeli(pudele: Pudele): Observable<HttpResponse<number>> {
+    return this.http.post<number>(`${this.URL}api/pudeles`, pudele, {
+      headers: { 'Content-Type': 'application/json' },
+      observe: 'response'
+    })
   }
 
 }
